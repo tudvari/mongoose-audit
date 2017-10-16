673cf8cdbc678c4cc9c5786506570b1f37c52ee4
Udvari Tibor <tudvari@tudvari.com>
2017-10-16 21:19:34 +0200

    init check, test refinements

--- tests/runner/saveTest.js
+++ tests/runner/saveTest.js
@@ -6,25 +6,27 @@ var blogSchema = new Schema({
 	name: String,
 	title: String
 })
+
 var connection = mongoose.createConnection('mongodb://localhost/test')
 
 audit.initialize(connection, 'Blog')
 blogSchema.plugin(audit.plugin)
 
+setTimeout(function () {
 
-var Blog = connection.model('Blog', blogSchema)
+	var Blog = connection.model('Blog', blogSchema)
+	var article = new Blog({title: 'test Title1', name: 'testname'})
 
-var article = new Blog({title: 'test Title1', name: 'lófasz'})
+	article.save()
 
-article.save()
+	Blog.find({}, function (err, result) {
+		if (err) throw err
+		console.log('found result: ', result)
+	})
 
-Blog.find({}, function (err, result) {
-	if (err) throw err
-	console.log('found result: ', result)
-})
-/*
-Blog.findOneAndUpdate({name: 'lófasz'}, {title: 'uj title'}, {upsert: true}, function (err, doc) {
-	if (err) throw err
-	console.log('updated.. ', doc)
-})
-*/
+	Blog.findOneAndUpdate({name: 'testname'}, {title: 'uj title'}, {upsert: true}, function (err, doc) {
+		if (err) throw err
+		console.log('updated.. ', doc)
+	})
+
+}, 5000)
