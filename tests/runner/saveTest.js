f4bcb3765e2f79a04a3239d11973857621e64758
Udvari Tibor <tudvari@tudvari.com>
2017-10-16 20:17:38 +0200

    optimize v2

--- tests/runner/saveTest.js
+++ tests/runner/saveTest.js
@@ -16,13 +16,13 @@ var Blog = mongoose.model('Blog', blogSchema)
 var article = new Blog({title: 'test Title1', name: 'lófasz'})
 
 article.save()
-
 Blog.find({}, function (err, result) {
 	if (err) throw err
 	console.log('found result: ', result)
 })
-
+/*
 Blog.findOneAndUpdate({name: 'lófasz'}, {title: 'uj title'}, {upsert: true}, function (err, doc) {
 	if (err) throw err
 	console.log('updated.. ', doc)
 })
+*/
